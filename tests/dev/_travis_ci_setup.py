bd5925ac5abd90344b3f2a5a9dc651a069a3b5e0
Peter Harrison <peter@colovore.com>
2019-10-14 11:04:51 -0700

    Added recursive unittesting

--- tests/dev/_travis_ci_setup.py
+++ tests/dev/_travis_ci_setup.py
@@ -7,13 +7,14 @@ import sys
 import os
 
 # Try to create a working PYTHONPATH
-TEST_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-ROOT_DIRECTORY = os.path.abspath(os.path.join(TEST_DIRECTORY, os.pardir))
-if TEST_DIRECTORY.endswith('/pattoo-agents/tests') is True:
-    sys.path.append(ROOT_DIRECTORY)
+DEV_DIR = os.path.dirname(os.path.realpath(__file__))
+ROOT_DIR = os.path.abspath(os.path.join(
+    os.path.abspath(os.path.join(DEV_DIR, os.pardir)), os.pardir))
+if DEV_DIR.endswith('/pattoo-agents/tests/dev') is True:
+    sys.path.append(ROOT_DIR)
 else:
     print(
-        'This script is not installed in the "pattoo-agents/bin" directory. '
+        'This script is not installed in the "tests/dev" directory. '
         'Please fix.')
     sys.exit(2)
 
---
+++ tests/test_files.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Test the files module."""
+
+# Standard imports
+import unittest
+import sys
+
+
+class TestBasicFunctions(unittest.TestCase):
+    """Checks all functions and methods."""
+
+    #########################################################################
+    # General object setup
+    #########################################################################
+
+    def test_root_directory(self):
+        """Testing function root_directory."""
+        pass
+
+    def test_read_yaml_files(self):
+        """Testing function read_yaml_files."""
+        pass
+
+    def test_read_yaml_file(self):
+        """Testing function read_yaml_file."""
+        pass
+
+    def test_mkdir(self):
+        """Testing function mkdir."""
+        pass
+
+
+if __name__ == '__main__':
+
+    # Do the unit test
+    unittest.main()
