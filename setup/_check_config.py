9454d0c51698d13491e9efc3a499fef6eb55452a
Peter Harrison <peter@colovore.com>
2019-12-05 19:49:33 -0400

    PattooShared v0.0.53

--- setup/_check_config.py
+++ setup/_check_config.py
@@ -62,7 +62,7 @@ Section "{}" not found in configuration file in directory {}. Please fix.\
     secondary_key_check(config, 'polling', secondaries)
 
     # Print Status
-    print('OK: Configuration paramter check passed.')
+    print('OK: Configuration parameter check passed.')
 
 
 def secondary_key_check(config, primary, secondaries):
